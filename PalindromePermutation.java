efe33edf583dbc1473032e79229c18218c84d5e2
Yuyang Yu <yuyang.yu105@gmail.com>
2017-12-17 01:02:48 -0800

    P266

---
+++ PalindromePermutation.java
@@ -0,0 +1,18 @@
+/**
+Given a string, determine if a permutation of the string could form a palindrome.
+
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+*/
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set=new HashSet<Character>();
+        for(int i=0; i<s.length(); ++i){
+            if (!set.contains(s.charAt(i)))
+                set.add(s.charAt(i));
+            else 
+                set.remove(s.charAt(i));
+        }
+        return set.size()==0 || set.size()==1;
+    }
+}
