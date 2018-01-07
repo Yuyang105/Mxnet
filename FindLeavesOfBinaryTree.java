96ebc8e4bc2d040a27310ea676893e1adc7a6e73
Yuyang Yu <yuyang.yu105@gmail.com>
2018-01-06 21:39:25 -0800

    p366

---
+++ FindLeavesOfBinaryTree.java
@@ -0,0 +1,60 @@
+/**
+Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+
+Example:
+Given binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Returns [4, 5, 3], [2], [1].
+
+Explanation:
+1. Removing the leaves [4, 5, 3] would result in this tree:
+
+          1
+         / 
+        2          
+2. Now removing the leaf [2] would result in this tree:
+
+          1          
+3. Now removing the leaf [1] would result in the empty tree:
+
+          []         
+Returns [4, 5, 3], [2], [1].
+
+Credits:
+Special thanks to @elmirap for adding this problem and creating all test cases.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(res, root);
+        return res;
+    }
+    
+    private int helper(List<List<Integer>> res, TreeNode root) {
+        if (root == null) return -1;
+        int left = helper(res, root.left);
+        int right = helper(res, root.right);
+        int level = Math.max(left, right) + 1;
+        if (res.size() == level) {
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(root.val);
+        root.left = null;
+        root.right = null;
+        return level;
+    }
+}
