a62c2bacfbd76921026260a3a49e0fd77b6887d8
Yuyang Yu <yuyang.yu105@gmail.com>
2018-01-07 20:31:30 -0800

    309

---
+++ BestTimeBuySellStockWithCD.java
@@ -0,0 +1,28 @@
+/**
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+Example:
+
+prices = [1, 2, 3, 0, 2]
+maxProfit = 3
+transactions = [buy, sell, cooldown, buy, sell]
+Credits:
+Special thanks to @dietpepsi for adding this problem and creating all test cases.
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sell = 0, pre_sell = 0, buy = Integer.MIN_VALUE, pre_buy = 0;
+        for (int price : prices) {
+            pre_buy = buy;
+            buy = Math.max(pre_sell - price, pre_buy);
+            pre_sell = sell;
+            sell = Math.max(pre_buy + price, pre_sell);
+        }
+        return sell;
+    }
+}
