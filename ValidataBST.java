89894ec946ebe410869265f9754aafd798eea101
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-11 16:15:08 -0800

    98

--- ValidataBST.java
+++ ValidataBST.java
@@ -28,20 +28,31 @@ Binary tree [1,2,3], return false.
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
+    public boolean isValidBST1(TreeNode root) {
         if (root == null) return true;
         Stack<TreeNode> stack = new Stack<>();
         TreeNode pre = null;
         while (root != null || !stack.isEmpty()) {
-            while (root!= null) {
+            while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
             root = stack.pop();
-            if (pre != null && root.val <= pre.val) return false;
+            if (pre != null && pre.val >= root.val) return false;
             pre = root;
             root = root.right;
         }
         return true;
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    private boolean helper(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+        return helper(node.left, min, node.val) &&
+            helper(node.right, node.val, max);
     }
 }
