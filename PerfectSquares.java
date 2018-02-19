a1ec61f7d32c41f96ae05874d699e7b4a179417a
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-18 18:05:56 -0800

    279

--- PerfectSquares.java
+++ PerfectSquares.java
@@ -1,4 +1,13 @@
 /**
+
+dp:
+初始化dp[0] = 0, 从 i = 0 到 i = n，从 j = 1 到 j * j <= i;
+dp[i] = min(dp[i], dp[i - j * j] + 1);
+
+*/
+
+
+/**
 Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
 For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
