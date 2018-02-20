adc2d06603fbf53a34dc2065ec5f19a6e1fb90ec
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-19 20:50:42 -0800

    606

--- ConstructStringFromBinaryTree.java
+++ ConstructStringFromBinaryTree.java
@@ -1,4 +1,12 @@
 /**
+
+老故事：递归/遍历。
+
+遍历使用stack。
+
+*/
+
+/**
 You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
 
 The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
@@ -39,6 +47,33 @@ except we can't omit the first parenthesis pair to break the one-to-one mapping 
  * }
  */
 class Solution {
+        // 用stack遍历
+    public String tree2str(TreeNode t) {
+        if (t == null)
+            return "";
+        Stack < TreeNode > stack = new Stack < > ();
+        stack.push(t);
+        Set < TreeNode > visited = new HashSet < > ();
+        StringBuilder s = new StringBuilder();
+        while (!stack.isEmpty()) {
+            t = stack.peek();
+            if (visited.contains(t)) {
+                stack.pop();
+                s.append(")");
+            } else {
+                visited.add(t);
+                s.append("(" + t.val);
+                if (t.left == null && t.right != null)
+                    s.append("()");
+                if (t.right != null)
+                    stack.push(t.right);
+                if (t.left != null)
+                    stack.push(t.left);
+            }
+        }
+        return s.substring(1, s.length() - 1);
+    }
+     
     public String tree2str(TreeNode t) {
         if (t == null) return "";
         if (t.left == null && t.right == null) return t.val + "";
