1cd23e328ea6b222c0f04ac121f6d2455ca68d2f
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-19 21:03:49 -0800

    462

--- MinimumMovesToEqualArrayElementsII.java
+++ MinimumMovesToEqualArrayElementsII.java
@@ -1,4 +1,13 @@
 /**
+
+题目大意：
+给定非空整数数组，求使得数组中的所有元素均相等的最小移动次数，一次移动是指将某个元素加1或者减1。
+
+排序之后，从两边往中间走，最大和最小之间的差距，是一定要填补上的，不管+1 还是 -1，所以最后都等于中位数。
+
+*/
+
+/**
 Given a non-empty integer array, find the minimum number of moves required to make all array elements equal, where a move is incrementing a selected element by 1 or decrementing a selected element by 1.
 
 You may assume the array's length is at most 10,000.
