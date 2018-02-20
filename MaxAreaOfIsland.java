0f0a53c8e43f0486b172bedaff343be5e8d09c51
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-19 20:09:44 -0800

    695

--- MaxAreaOfIsland.java
+++ MaxAreaOfIsland.java
@@ -1,4 +1,12 @@
 /**
+
+题意：给定一个二维数组，计算其中最大岛屿的面积。（注意形成岛屿的规则）
+
+把问题当做图来做，使用visited记录访问状态，使用DFS，回溯法解决问题。
+
+*/
+
+/**
 Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
 
 Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
