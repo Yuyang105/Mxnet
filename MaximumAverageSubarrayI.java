6cb081cf73a0a271d8eeb04b2a553bacdd55df29
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-19 20:31:51 -0800

    643

--- MaximumAverageSubarrayI.java
+++ MaximumAverageSubarrayI.java
@@ -1,4 +1,13 @@
 /**
+
+给一个有n个元素的数组，找其中平均值最大的长度为k的子串，要求返回最大的平均值。
+先算出前k个元素的sum，之后依次向右枚举，每次删掉最左边的元素，加上最右边的元素，相当于窗口移动，
+并在每次移动时，更新maxSum。当枚举结束后，返回 maxSum / k。
+
+*/
+
+
+/**
 Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. And you need to output the maximum average value.
 
 Example 1:
