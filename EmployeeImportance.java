66711395d1962ed7a4c16a9ee0666c483452f21b
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-19 20:17:13 -0800

    690

--- EmployeeImportance.java
+++ EmployeeImportance.java
@@ -1,4 +1,13 @@
 /**
+
+题意：规定了一个员工信息的数据结构，包含了雇员的id(唯一)、重要值和他的直系下属的雇员id。
+一个员工至多只有一个直系领导，但是可以有多个下属。求出给定一个员工集合及领导的id，求出领导及其所有下属的重要值总和。
+
+用hashMap将id与其对应的数据结构映射起来，找到id对应的员工信息并将其加入到一个队列中。
+
+*/
+
+/**
 You are given a data structure of employee information, which includes the employee's unique id, his importance value and his direct subordinates' id.
 
 For example, employee 1 is the leader of employee 2, and employee 2 is the leader of employee 3. They have importance value 15, 10 and 5, respectively. Then employee 1 has a data structure like [1, 15, [2]], and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. Note that although employee 3 is also a subordinate of employee 1, the relationship is not direct.
