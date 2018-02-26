9e59d061bc1f15ac1874782babc20b5ce0bc5274
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-25 21:13:39 -0800

    136

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,3 +1,20 @@
+/**
+
+位运算： 异或  ->    相同为0，不同为1.
+                      1 1 : 0
+                      0 0 : 0           ->  N ^ N = 0
+                      1 0 : 1           
+                      0 1 : 1           ->  0 ^ N = N
+        所以：
+        if N is the single number
+        N1 ^ N1 ^ N2 ^ N2 ^…^ Nx ^ Nx ^ N
+        = (N1^N1) ^ (N2^N2) ^…^ (Nx^Nx) ^ N
+        = 0 ^ 0 ^ …^ 0 ^ N
+        = N                  
+        Time: O(n), Space: O(1).
+
+*/
+
 class Solution {
     public int singleNumber(int[] nums) {
         int magic = 0;
