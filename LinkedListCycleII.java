31e6cfe6304dd007a11b164d042fe079f4dd41fe
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-26 22:05:17 -0800

    142

--- LinkedListCycleII.java
+++ LinkedListCycleII.java
@@ -8,6 +8,30 @@ Can you solve it without using extra space?
 */
 
 /**
+
+找环的起点：
+
+X     a     Y         b
+·-----------·-------·
+            |       |
+            |       · Z
+          c |       |
+            |       |
+            ·-------·
+            
+如图，出发点为X，环的起点为Y，Fast和Slow相遇于Z。期间距离为a, b, c.
+
+Fast速度是Slow的两倍 => Fast走过的距离是Slow的两倍 
+                   => a + b + c + b = 2 (a + b)
+                   => a = c
+
+由于[X到Y的距离a]等于[Z到Y的距离c]，再派一个Slow走X出发，原Slow继续走，它俩相逢点就是Y。
+
+
+*/
+
+
+/**
  * Definition for singly-linked list.
  * class ListNode {
  *     int val;
@@ -20,41 +44,21 @@ Can you solve it without using extra space?
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return null;
-        }
+        if (head == null) return null;
         
-        ListNode fast = new ListNode(0);
-        ListNode slow = new ListNode(0);
-        slow = head;
-        fast = head.next;
-        
-        while (fast != slow) {
-            if (fast == null || fast.next == null) {
-                return null;
-            }
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+            if (fast == slow) {
+                ListNode res = head;
+                while (res != slow) {
+                    res = res.next;
+                    slow = slow.next;
+                }
+                return res;
+            }
         }
-        
-        while (head != slow.next) {
-            head = head.next;
-            slow = slow.next;
-        }
-        return head;
-        
-        // while (fast != null && fast.next != null) {
-        //     fast = fast.next.next;
-        //     slow = slow.next;
-        //     if (fast == slow) {
-        //         slow = head;
-        //         while (head != slow.next) {
-        //             slow = slow.next;
-        //             head = head.next;
-        //         }
-        //         return head;
-        //     }
-        // }
-        // return null;
+        return null;
     }
 }
