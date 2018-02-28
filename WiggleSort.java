f1cf611e6c97d74e64648ec76fabd035a679c5a4
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-27 22:22:02 -0800

    280

--- WiggleSort.java
+++ WiggleSort.java
@@ -1,4 +1,12 @@
 /**
+等于冒泡交换，one-pass，直接看是奇数位，就应该比前一个数大，否则应该小于前一个数。不满足就交换。
+
+时间复杂度: O(n)
+空间复杂度: O(1)
+
+*/
+
+/**
 Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
 
 For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
