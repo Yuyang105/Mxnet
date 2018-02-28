0806e9392fc51a68d9cfb92107a5d167c8c65273
Yuyang Yu <yuyang.yu105@gmail.com>
2018-02-27 17:51:07 -0800

    74

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -1,15 +1,31 @@
+/**
+
+由于从左到右是排好序的，并且每行行首比上一行行位要大，也就是完全排好序了。
+那么我们可以扯直了，直接做二分法。
+
+看作是长度是 row * col 的数组，index / col => row, index % col => col。
+
+*/
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
-        int start = 0, end = matrix.length * matrix[0].length - 1;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (matrix[mid / matrix[0].length][mid % matrix[0].length] == target) return true;
-            else if (matrix[mid / matrix[0].length][mid % matrix[0].length] > target) end = mid;
-            else start =  mid;
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0)
+            return false;
+        
+        // 二分法，row.len = matrix[0].length
+        int low = 0, high = matrix.length * matrix[0].length - 1;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[mid / matrix[0].length][mid % matrix[0].length] == target)
+                return true;
+            if (matrix[mid / matrix[0].length][mid % matrix[0].length] < target)
+                low = mid;
+            else
+                high = mid;
         }
-        if (matrix[end / matrix[0].length][end % matrix[0].length] == target) return true;
-        else if (matrix[start / matrix[0].length][start % matrix[0].length] == target) return true;
-        else return false;
+        if (matrix[low / matrix[0].length][low % matrix[0].length] == target)
+            return true;
+        if (matrix[high / matrix[0].length][high % matrix[0].length] == target)
+            return true;
+        return false;
     }
 }
