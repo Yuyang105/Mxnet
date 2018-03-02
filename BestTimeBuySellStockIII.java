64ec07d98ccdf0dfd177a68810999fff3be6cb99
Yuyang Yu <yuyang.yu105@gmail.com>
2018-03-01 22:22:29 -0800

    123

--- BestTimeBuySellStockIII.java
+++ BestTimeBuySellStockIII.java
@@ -1,4 +1,15 @@
 /**
+
+这个题目trick的地方在于，我们抓buy1, buy2, sell1, sell2的时候，我们是倒过来处理的，可以这么理解：
+
+basically we want to maximize the formula: -price1 + price2 - price3 + price4, 
+where every price appear in that order. So every time we visit a new price, 
+we update the max possible value of 4 (partial) formula. That’s why we use max for each of the 4 states.
+
+*/
+
+
+/**
 Say you have an array for which the ith element is the price of a given stock on day i.
 
 Design an algorithm to find the maximum profit. You may complete at most two transactions.
