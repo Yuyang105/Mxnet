f4cd71f257d7cc6fe23c0c0786ddfc73965cac82
Yuyang Yu <yuyang.yu105@gmail.com>
2018-03-01 23:06:06 -0800

    344

--- ReverseString.java
+++ ReverseString.java
@@ -1,5 +1,18 @@
 class Solution {
-    public String reverseString(String s) {
+    public String reverseString1(String s) {
         return new StringBuilder(s).reverse().toString();
+    }
+    
+    public String reverseString(String s) {
+        if (s == null || s.length() == 0) return s;
+        int left = 0;
+        int right = s.length() - 1;
+        char[] str = s.toCharArray();
+        while (left < right) {
+            char temp = str[left];
+            str[left++] = str[right];
+            str[right--] = temp;
+        }
+        return new String(str);
     }
 }
